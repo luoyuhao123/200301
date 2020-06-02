5a5d385dfbf23188da791a1a8f860c5b1e462291
Administrator <Administrator@SC-201802131950>
2020-06-02 16:10:31 +0800

    "2020-06-02：创建项目"

---
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/SpringBootDemo200301Application.java
@@ -0,0 +1,13 @@
+package com.hqyj.SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo200301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo200301Application.class, args);
+	}
+
+}
