c800482b61cd75c4c51a2aa7542532eba91d7a8b
luoyuhao123 <1114546242@qq.com>
2020-06-11 11:36:28 +0800

    "2020-6-11 注册页面，静态资源"

--- SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/account/controller/AccountController.java
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/account/controller/AccountController.java
@@ -7,8 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
+	/**
+	 * 127.0.0.1/account/login
+	 */
 	@RequestMapping("/login")
 	public String loginPage() {
+		return "indexSimple";
+	}
+	
+	/**
+	 * 127.0.0.1/account/register
+	 */
+	@RequestMapping("/register")
+	public String registerPage() {
 		return "indexSimple";
 	}
 }
